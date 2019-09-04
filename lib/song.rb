601bf51fbce9158c8df6ae39d6b5510068600989
Hanna Lee <mani21na@gmail.com>
2019-09-04 22:15:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,24 +1,6 @@
 #require 'pry'
 class Song
   attr_accessor :name, :artist, :genre
-  attr_reader
-  
-  @@all = []
-
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @artist = genre
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def genre#require 'pry'
-class Song
-  attr_accessor :name, :artist, :genre
   
   @@all = []
 
@@ -40,11 +22,4 @@ class Song
   #def artist
   #  self.find_all{|song| song.artist == self.artist}
   #end
-end
-    self.all.find{|song| song.genre == self}
-  end
-
-  def artist
-    self.all.find{|song| song.artist == self}
-  end
 end
